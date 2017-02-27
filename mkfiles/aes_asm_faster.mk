68eb6a474ee071a9f11a54d2e0f4abeaccbdfc08
Tobias Schramm <tobleminer@gmail.com>
2017-02-27 15:15:11 +0100

    Add bcal OBJs to compile target

--- mkfiles/aes_asm_faster.mk
+++ mkfiles/aes_asm_faster.mk
@@ -8,7 +8,7 @@ BLOCK_CIPHERS += $(ALGO_NAME)
 $(ALGO_NAME)_DIR      := aes/
 $(ALGO_NAME)_INCDIR   := memxor/ gf256mul/ bcal/
 $(ALGO_NAME)_OBJ      := aes_enc-asm.o aes_dec-asm_faster.o aes_sbox-asm.o aes_invsbox-asm.o  \
-                         aes_keyschedule-asm.o
+                         aes_keyschedule-asm.o bcal_aes128.o bcal-cbc.o bcal-basic.o memxor.o keysize_descriptor.o
 $(ALGO_NAME)_TESTBIN  := main-aes-test.o $(CLI_STD) $(BCAL_STD)  \
                          bcal_aes128.o bcal_aes192.o bcal_aes256.o bcal-cbc.o \
                          dump-asm.o dump-decl.o bcal-cfb_byte.o       \
